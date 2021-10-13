9467e2bd0b6c560f9f4607b0161f8fbc8e9933b2
Akari Oomori <oomori.akari7766@gmail.com>
2021-10-13 23:33:15 +0900

    add:typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,14 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: "gatsby-plugin-typescript",
+      options: {
+        isTSX: true,
+        jsxPragma: "react-jsx",
+        allExtensions: true,
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+class Index extends React.Component {
+  render() {
+    return <div>Hello world!</div>
+  }
 }
+
+export default Index
